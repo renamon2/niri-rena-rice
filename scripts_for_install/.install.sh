908041298656a6ea9e230a1b8a6f80714e28e9c0
renamon2 <ll4r1nall@gmail.com>
2026-07-03 23:27:36 +0300

    niri auto cfg install

---
+++ scripts_for_install/.install.sh
@@ -0,0 +1,43 @@
+echo -e "Hey, look who made it to the best part—it’s you, gorgeous. \nSo, let’s get *niri* set up just for you. You don’t mind, do you? \nJust a heads-up: if you press Ctrl+C, it’ll stop, and you’ll have to start all over again..."
+URL=https://raw.githubusercontent.com/renamon2/niri-rena-rice/refs/heads/main/files/config.kdl
+DEST_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/niri"
+CFG="$DEST_DIR/config.kdl"
+if [ -d "$CFG" ]; then
+    mv "$CFG" "$CFG.bak"
+    echo "niri config.kdl has been backed up to $CFG.bak."
+else
+    mkdir -p "$DEST_DIR"
+    echo "$DEST_DIR directory created."
+fi
+
+if [ -d "$DEST_DIR" ] && niri &> /dev/null; then
+    curl -sSL "$URL" -o "$DEST_DIR/config.kdl"
+    echo "$CFG has been installed."
+else
+    echo "Hey, this is part of the script at https://github.com/renamon2/niri-rena-rice/tree/main. Use the command in the README.md."
+    sleep 7
+    xdg-open "https://github.com/renamon2/niri-rena-rice/tree/main"
+    exit 1
+fi
+
+sed "s/user/$USER/g" "$CFG" > "$CFG.tmp" && mv "$CFG.tmp" "$CFG"
+echo "$CFG has been updated."
+URL=https://githubusercontent.com/renamon2/niri-rena-rice/raw/refs/heads/main/help.tar.gz
+DEST_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/niri/help"
+if [ -d "$DEST_DIR" ]; then
+    curl -sSL "$URL" -o "$DEST_DIR/help.tar.gz"
+    echo "niri help has been installed."
+    tar -xzf "$DEST_DIR/help.tar.gz" -C "$DEST_DIR"
+    rm "$DEST_DIR/help.tar.gz"
+    echo "niri help has been extracted."
+    echo "niri help has been updated. When you start niri with new settings, then run Super+F1."
+else
+    mkdir -p "$DEST_DIR"
+    echo "$DEST_DIR directory created."
+    curl -sSL "$URL" -o "$DEST_DIR/help.tar.gz"
+    echo "niri help has been installed."
+    tar -xzf "$DEST_DIR/help.tar.gz" -C "$DEST_DIR"
+    rm "$DEST_DIR/help.tar.gz"
+    echo "niri help has been extracted."
+    echo "niri help has been updated. When you start niri with new settings, then run Super+F1."
+fi
