0d47e17fe536769dae0bc35105134c27cc4555a0
renamon2 <ll4r1nall@gmail.com>
2026-07-16 20:45:28 +0300

    add support sddm with run it

--- scripts_for_install/.install.sh
+++ scripts_for_install/.install.sh
@@ -2,6 +2,8 @@ echo -e "Hey, look who made it to the best part—it’s you, gorgeous. \nSo, le
 URL=https://raw.githubusercontent.com/renamon2/niri-rena-rice/refs/heads/main/files/config.kdl
 DEST_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/niri"
 CFG="$DEST_DIR/config.kdl"
+#Бэкап существующей конфигурации, если существует
+# EN: Backup existing configuration, if it exists
 if [ -d "$CFG" ]; then
     mv "$CFG" "$CFG.bak"
     echo "niri config.kdl has been backed up to $CFG.bak."
@@ -9,7 +11,8 @@ else
     mkdir -p "$DEST_DIR"
     echo "$DEST_DIR directory created."
 fi
-
+# Установка конфигурации
+# EN: Install configuration
 if [ -d "$DEST_DIR" ] && command -v niri >/dev/null 2>&1; then
     curl -sSL "$URL" -o "$DEST_DIR/config.kdl"
     echo "$CFG has been installed."
@@ -19,7 +22,8 @@ else
     xdg-open "https://github.com/renamon2/niri-rena-rice/tree/main"
     exit 1
 fi
-
+# Добавление help.html
+# EN: Add help.html
 sed "s/user/$USER/g" "$CFG" > "$CFG.tmp" && mv "$CFG.tmp" "$CFG"
 echo "$CFG has been updated."
 URL=https://githubusercontent.com/renamon2/niri-rena-rice/raw/refs/heads/main/help.tar.gz
@@ -41,9 +45,35 @@ else
     echo "niri help has been extracted."
     echo "niri help has been updated. When you start niri with new settings, then run Super+F1."
 fi
+# Добавление обоев
+# EN: Add wallpapers
 URL1=https://raw.githubusercontent.com/renamon2/niri-rena-rice/refs/heads/main/assets/toki_in_space-0.3_overview.png
 URL2=https://raw.githubusercontent.com/renamon2/niri-rena-rice/refs/heads/main/assets/toki_in_space-blurred.png
 DEST_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/niri/wallpaper/awww/"
 mkdir -p $DEST_DIR
 curl -sSL "$URL1" -o "$DEST_DIR/toki_in_space-0.3_overview.png"
 curl -sSL "$URL2" -o "$DEST_DIR/toki_in_space-blurred.png"
+echo "niri wallpapers have been installed."
+
+# Добавляем niri-rice.desktop, если systemctl не найден
+# EN: Add niri-rice.desktop, if systemctl is not found
+DEST_DIR="${XDG_DATA_DIRS:-/usr/local/share:/usr/share}/wayland-sessions/"
+if command -v systemctl --version &>/dev/null; then
+    echo "systemctl found. Skipping niri-rice.desktop creation for runit."
+else
+    echo "systemctl not found. Creating niri-rice.desktop for runit."
+    touch "$DEST_DIR/niri-rice.desktop"
+    echo "niri-rice.desktop has been created."
+    sudo tee "$DEST_DIR/niri-rice.desktop" > /dev/null <<EOF
+[Desktop Entry]
+Name=Niri (Rice)
+Comment=A scrollable-tiling Wayland compositor
+Exec=$HOME/.config/niri/niri-wrapper.sh
+Type=Application
+DesktopNames=rice-niri
+EOF
+    echo "niri-rice.desktop has been written."
+    sudo chmod 644 "$DEST_DIR/niri-rice.desktop"
+    sudo chown root:root "$DEST_DIR/niri-rice.desktop"
+    echo "niri-rice.desktop ownership has been set."
+fi
